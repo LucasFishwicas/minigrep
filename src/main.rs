257e00901bfe0c68cf58051e7e585768da5033ed
LucasFishwicas <luke-fishwick_@hotmail.com>
2023-05-20 12:19:40 -0400

    created lib.rs to separate functionality from main()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env; // allows us to access the args iterator
-use std::fs; // allows us to us the file system
 use std::process; // allows us to manipulate the running process 
-use std::error::Error; // allows us to access the Error trait object
+
+use minigrep::Config;
 
 fn main() {
     // store arguments in args - those given when 'cargo run' is called
@@ -20,42 +20,10 @@ fn main() {
 
     // execute run, which reads and displays the given files contents
     // or display an error message and end the program
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}",e);
         process::exit(1);
     }
         
 }
 
-
-// reads and displays the given files contents returns an error
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{}",contents);
-
-    Ok(())
-}
-
-
-// Config struct to hold the query content collected from args
-struct Config {
-    query: String,
-    file_path: String
-}
-
-
-// Config methods
-impl Config {
-    // constructor which will return an error if there are less than 3 args
-    fn build(args: &[String]) -> Result<Config, &'static str> { 
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone(); 
-        let file_path = args[2].clone();
-
-        Ok(Config {query,file_path})
-    }
-}
